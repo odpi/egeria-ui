a1a15bb88f91aae425bc81df16d6f44d88d192f2
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2020-11-24 18:09:15 +0200

    Update user icon
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- src/user-options-menu.js
+++ src/user-options-menu.js
@@ -11,11 +11,11 @@ import '@vaadin/vaadin-grid/vaadin-grid.js';
 import '@vaadin/vaadin-grid/vaadin-grid-selection-column.js';
 import '@vaadin/vaadin-grid/vaadin-grid-sort-column.js';
 
-import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 
 class UserOptions extends PolymerElement {
-    static get template() {
-        return html`
+  static get template() {
+    return html`
       <style>
         .avatar {
           height: 40px;
@@ -24,26 +24,33 @@ class UserOptions extends PolymerElement {
           box-sizing: border-box;
           background-color: #DDD;
         }
-        
-        paper-item a{
-            text-decoration: none;
-            color: var(--egeria-secondary-color);
+
+        paper-item a {
+          text-decoration: none;
+          color: var(--egeria-secondary-color);
         }
+
         paper-button {
           text-transform: none;
         }
       </style>
-        <token-ajax id="userAjax" last-response="{{user}}" url="/api/users/current" auto></token-ajax>
-        <div style="float: right">
-            <paper-menu-button
-                horizontal-align="right"
-                horizontal-offset="20" 
-                horizontal-align="bottom"
-                vertical-offset="65" 
-                style="margin-top: 10px">
+      <token-ajax id="userAjax" last-response="{{user}}" url="/api/users/current" auto></token-ajax>
+      <div style="float: right">
+        <paper-menu-button horizontal-align="right"
+                           horizontal-offset="20"
+                horizontal-offset="20"
+                           horizontal-offset="20"
+                           horizontal-align="bottom"
+                           vertical-offset="65"
+                vertical-offset="65"
+                           vertical-offset="65"
+                           style="margin-top: 10px">
             <paper-icon-item slot="dropdown-trigger">
-                <div class="avatar" slot="item-icon"></div>
+              <div class="avatar" slot="item-icon">
+                <img src="./icons/user.svg" height="100%"/>
+              </div>
             </paper-icon-item>
+
             <paper-listbox slot="dropdown-content" style="min-width: 200px">
                 <paper-item>Signed in as:<br> [[user.username]]</paper-item>
                 <hr>
@@ -54,33 +61,34 @@ class UserOptions extends PolymerElement {
                 <paper-item><a href="[[rootPath]]#/about">About</a></paper-item>
                 <hr>
                 <paper-item>
-                    <paper-button on-tap="_logout" icon="exit-to-app" title="Exit">Sign out</paper-button>
+                  <paper-button on-tap="_logout" icon="exit-to-app" title="Exit">
+                    Sign out
+                  </paper-button>
                 </paper-item>
              </paper-listbox>
         </paper-menu-button>
       </div>
-       
+
     `;
-    }
+  }
 
-    static get properties() {
-        return {
-            user: {
-                type: Object,
-                notify: true
-            }
-        };
-    }
+  static get properties() {
+    return {
+      user: {
+        type: Object,
+        notify: true
+      }
+    };
+  }
 
-    _logout(){
-        var customEvent = new CustomEvent('logout', {
-            bubbles: true,
-            composed: true,
-            detail: {greeted: "Bye!"}
-        });
-        this.dispatchEvent(customEvent);
-    }
-
+  _logout() {
+    var customEvent = new CustomEvent('logout', {
+      bubbles: true,
+      composed: true,
+      detail: { greeted: "Bye!" }
+    });
+    this.dispatchEvent(customEvent);
+  }
 }
 
 window.customElements.define('user-options', UserOptions);
