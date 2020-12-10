1e6ce2a38584ee1a968139c6f7e08cd69fc77dba
Bogdan Sava <bogdan.sava@gmail.com>
2020-12-10 17:01:16 +0200

    add feedback message for no more data
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- src/asset-catalog/asset-search-view.js
+++ src/asset-catalog/asset-search-view.js
@@ -208,6 +208,10 @@ class AssetSearchView extends mixinBehaviors([AppLocalizeBehavior], PolymerEleme
         }
         if( items === null || items.length ===0 ){
             this.$.more.disabled = true;
+            this.dispatchEvent(new CustomEvent('show-modal', {
+                bubbles: true,
+                composed: true,
+                detail: { message: "No more metadata to fetch for this criteria!", level: 'info'}}));
         } else {
             this.$.more.disabled = false;
         }
