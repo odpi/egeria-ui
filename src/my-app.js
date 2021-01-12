fa5de29b6b3721acaa716b347d108c1c2a908903
Bogdan Sava <bogdan.sava@gmail.com>
2021-01-12 12:26:29 +0200

    redirect to landing page
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- src/my-app.js
+++ src/my-app.js
@@ -381,6 +381,7 @@ class MyApp extends mixinBehaviors([AppLocalizeBehavior, RoleComponentsBehavior]
     if (!page) {
       if ((!!components && components.includes('asset-catalog')) || (!!components && components.length === 0)) {
         this.page = 'asset-catalog';
+        window.location = "/#/asset-catalog/search";
       } else {
         this.page = 'home-page';
       }
