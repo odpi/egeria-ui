82f51fae97a63fc0310dae9ebabf820c455d867d
Bogdan Sava <bogdan.sava@gmail.com>
2021-02-24 17:31:11 +0200

    remove commented code
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- src/common/item.js
+++ src/common/item.js
@@ -53,36 +53,6 @@ export const ItemViewBehaviorImpl = {
       return 'N/A';
   },
 
-  // _attributes(obj) {
-  //   var arr = [];
-  //   if (obj === undefined) {
-  //     return arr;
-  //   }
-  //   Object.keys(obj).forEach(
-  //     (key) => {
-  //       var value = obj[key];
-  //       if (typeof value !== 'object' && value !== null)
-  //         arr.push({ 'key': this._camelCaseToSentence(key), 'value': value });
-  //     }
-  //   );
-  //   return arr;
-  // },
-  //
-  // _hasKeys(obj) {
-  //   return Object.keys(obj).length > 0;
-  // },
-  //
-  // _hasKey(obj, key) {
-  //   return key in obj;
-  // },
-  //
-  // _camelCaseToSentence(val) {
-  //   return val
-  //     .replace(/([A-Z])/g, ' $1')
-  //     .replace(/([A-Z]+\s+)/g, c => c.trim())
-  //     .replace(/_/g, ' ') // replace underscores with spaces
-  //     .replace(/^\w/, c => c.toUpperCase()); //uppercase first letter
-  // }
 }
 
 export const ItemViewBehavior = [ ItemUtilsBehavior, ItemViewBehaviorImpl ]
