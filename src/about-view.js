7596e32f8e215efa03b7d428d2de168488917d25
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2021-04-06 10:49:08 +0300

    Update about page
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- src/about-view.js
+++ src/about-view.js
@@ -20,45 +20,38 @@ class AboutView extends PolymerElement {
           background-color: var(--egeria-background-color);
         }
       </style>
-    
-      <token-ajax id="userAjax" last-response="{{a}}" url="/api/about" auto></token-ajax>
-       
-        <div role="listbox">
-            <paper-item>
-              <paper-item-body two-line>
-                <div>Application name</div>
-                <div secondary>[[a.name]]</div>
-              </paper-item-body>
-            </paper-item>
-            
-            <paper-item>
-              <paper-item-body two-line>
-                <div>Maven group</div>
-                <div secondary>[[a.group]]</div>
-              </paper-item-body>
-            </paper-item>
-            
-             <paper-item>
-              <paper-item-body two-line>
-                <div>Maven artifact id</div>
-                <div secondary>[[a.artifact]]</div>
-              </paper-item-body>
-            </paper-item>
-            
-            <paper-item>
-              <paper-item-body two-line>
-                <div>Version</div>
-                <div secondary>[[a.version]]</div>
-              </paper-item-body>
-            </paper-item>
-            
-             <paper-item>
-              <paper-item-body two-line>
-                <div>Build time</div>
-                <div secondary>[[a.time]]</div>
-              </paper-item-body>
-            </paper-item>
-         </div>
+
+      <token-ajax id="about-request" last-response="{{ about }}" url="/about.json" auto></token-ajax>
+
+      <div role="listbox">
+        <paper-item>
+          <paper-item-body two-line>
+            <div>Application name</div>
+            <div secondary>[[ about.name ]]</div>
+          </paper-item-body>
+        </paper-item>
+
+        <paper-item>
+          <paper-item-body two-line>
+            <div>Version</div>
+            <div secondary>[[ about.version ]]</div>
+          </paper-item-body>
+        </paper-item>
+
+          <paper-item>
+          <paper-item-body two-line>
+            <div>Build time</div>
+            <div secondary>[[ about.buildTime ]]</div>
+          </paper-item-body>
+        </paper-item>
+
+        <paper-item>
+          <paper-item-body two-line>
+            <div>Commit ID</div>
+            <div secondary>[[ about.commitId ]]</div>
+          </paper-item-body>
+        </paper-item>
+      </div>
     `;
   }
 }
