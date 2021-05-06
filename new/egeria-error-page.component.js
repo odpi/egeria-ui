96c7eed5b9371c6bb114e81c9ecdbbe12e8c6134
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2021-05-06 17:19:41 +0300

    Added go home button on error pages
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- new/egeria-error-page.component.js
+++ new/egeria-error-page.component.js
@@ -3,6 +3,7 @@
 
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import '@polymer/iron-icon/iron-icon';
+import '@polymer/paper-button/paper-button';
 import '../old/shared-styles.js';
 
 class EgeriaErrorPage extends PolymerElement {
@@ -41,12 +42,33 @@ class EgeriaErrorPage extends PolymerElement {
           height:100px;
           width:100px;
         }
+
+        .go-home {
+          height: 24px;
+        }
+
+        .styled {
+          width:50%;
+          border:1px solid var(--egeria-primary-color);
+          padding:5px;
+          border-radius:5px;
+        }
+
+        .mt15 {
+          margin-top:15px;
+        }
       </style>
 
       <div class="content">
         <iron-icon icon="icons:report" class="bigger-icon"></iron-icon>
         <h1>[[ statusCode ]]</h1>
         <h2>[[ message ]]</h2>
+        <div class="mt15">
+          <a href="/" class="styled">
+            <iron-icon icon="icons:home"></iron-icon>
+            <span>Go home</span>
+          </a>
+        </div>
       </div>
     `;
   }
