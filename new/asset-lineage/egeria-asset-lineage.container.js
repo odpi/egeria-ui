3df689bacb4acaa87f2bd50d2c025d1312305150
Bogdan Sava <bogdan.sava@gmail.com>
2021-06-09 11:48:19 +0300

    fallbackDisplayName
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- new/asset-lineage/egeria-asset-lineage.container.js
+++ new/asset-lineage/egeria-asset-lineage.container.js
@@ -114,7 +114,7 @@ class EgeriaAssetLineage extends mixinBehaviors([EgeriaItemUtilsBehavior, RoleCo
               { href: null, name: 'asset-lineage' },
               {
                 href: `/asset-catalog/${ this.guid }/details`,
-                name: this.selectedNodeDetails.properties.displayName
+                name: this._fallbackDisplayName( this.item)
               },
               { href: `/asset-lineage/${ this.guid }/${ this.page }`, name: this.page }
             ]);
