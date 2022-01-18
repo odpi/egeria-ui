dd5e26611c25d3c3e71459992c00a3b2b6216e04
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2022-01-18 16:09:48 +0200

    Remove debugger
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- react-ui/src/helpers/handle-response.ts
+++ react-ui/src/helpers/handle-response.ts
@@ -1,8 +1,6 @@
 import { authenticationService } from '../services/authentication.service';
 
 export function handleResponse(response: any) {
-    debugger;
-
     if (!response.ok) {
         if ([401, 403].indexOf(response.status) !== -1) {
             // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
