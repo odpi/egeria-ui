b18d293d6f7602358724afc61cbda5d3a2bd6bfd
Bogdan Sava <bogdan.sava@gmail.com>
2022-02-17 12:23:55 +0200

    remove unused import
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- react-ui/src/index.tsx
+++ react-ui/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-// import Login from './components/Login/index';
 import SignIn from './components/Login/SignIn';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
