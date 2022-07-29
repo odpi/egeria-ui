7688aafa235bf88b28a87ba0b7465d1e19fd9885
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2022-07-29 16:00:40 +0300

    Implement main menu from props
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- src/components/AppInstance/index.tsx
+++ src/components/AppInstance/index.tsx
@@ -1,14 +1,23 @@
 import { EgeriaApp, RequireAuth, EgeriaAbout } from 'egeria-ui-core';
 import { EgeriaGlossary, EgeriaLineage } from 'egeria-ui-components';
 import { Routes, Route } from 'react-router-dom';
+import { apiUrl, menuIcons } from 'egeria-js-commons';
+
+const menu = [
+  { customIcon: menuIcons.assets, label: 'Asset Lineage', href: '/lineage' },
+  { customIcon: menuIcons.glossary, label: 'Glossary View', href: '/glossary' },
+  { customIcon: menuIcons.typeExplorer, label: 'Type Explorer', href: '/type-explorer' },
+  { customIcon: menuIcons.assetCatalog, label: 'Asset Catalog', href: '/assets/catalog' },
+  { customIcon: menuIcons.repositoryExplorer, label: 'Repository Explorer', href: '/repository-explorer' }
+];
 
 export function AppInstance() {
   return <>
-    <EgeriaApp main={
+    <EgeriaApp menu={menu} main={
       <Routes>
           <Route path="/hi" element={<>Hi</>} />
 
-          <Route path={"/about"} element={<EgeriaAbout />} />
+          <Route path={"/about"} element={<EgeriaAbout apiUrl={`${apiUrl()}`} />} />
 
           {/* <Route path={"/assets/:uuid/details"}
                   element={<RequireAuth><AssetDetails match={""} /></RequireAuth>} /> */}
